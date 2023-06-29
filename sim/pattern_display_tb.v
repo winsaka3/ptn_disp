94c024a83d6c870eabe1ac139a216ec0ae76035f
winsaka3 <y.katsusaka@gmail.com>
2023-06-29 11:36:57 +0900

    テストベンチにファイル出力を追加

--- sim/pattern_display_tb.v
+++ sim/pattern_display_tb.v
@@ -5,7 +5,7 @@ module pattern_display_tb;
   
   // Define clock period and number of clocks for simulation
   localparam STEP = 8;      // CLK period
-  localparam FRAME_NUM = 60; // Number of frames to simulate
+  localparam FRAME_NUM = 1; // Number of frames to simulate
   localparam CLKNUM = (H_SYNC_INTERVAL * V_SYNC_INTERVAL + 12000) * 5 * FRAME_NUM;
 
   // Connect Modules
@@ -47,13 +47,25 @@ module pattern_display_tb;
       CLK = 1; #(STEP/2);
   end
 
+  integer fd; // file variable
+
   // Create input to validation target
   initial begin
                   RST = 0;
+      fd = $fopen("imagedata.bin", "wb");
       #(STEP*600) RST = 1;
       #(STEP*20)  RST = 0;
       #(STEP*CLKNUM);
+      $fclose(fd);
       $stop;
   end
 
+  // File output
+  always @(posedge PCK) begin
+    if ( VGA_DISPLAY_EN ) begin
+        $fwrite(fd, "%c", VGA_R);
+        $fwrite(fd, "%c", VGA_G);
+        $fwrite(fd, "%c", VGA_B);
+    end
+  end
 endmodule
